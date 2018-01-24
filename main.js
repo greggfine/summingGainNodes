53bfb374431111e6fb4f6ed92dff238c5e078987
Gregg Fine <bbeez8500@gmail.com>
2018-01-24 17:38:32 -0500

    Improved variable names

--- main.js
+++ main.js
@@ -1,76 +1,72 @@
-const ctx = new AudioContext();
+"use strict";
+const ctx = new (window.AudioContext || window.webkitAudioContext)();
 
-const btn = document.getElementById('play');
-btn.addEventListener('click', play);
+const btn = document.getElementById("play");
+btn.addEventListener("click", play);
 
-const stopBtn = document.getElementById('stop');
-stopBtn.addEventListener('click', stop);
+const stopBtn = document.getElementById("stop");
+stopBtn.addEventListener("click", stop);
 
-const sineRange = document.getElementById('sine-range');
-const triRange = document.getElementById('tri-range');
-const squareRange = document.getElementById('square-range');
-const sawRange = document.getElementById('saw-range');
-const masterFilt = document.getElementById('master-filter');
-const masterOut = document.getElementById('master-out');
+const sineRange = document.getElementById("sine-range");
+const triRange = document.getElementById("tri-range");
+const squareRange = document.getElementById("square-range");
+const sawRange = document.getElementById("saw-range");
+const masterFilt = document.getElementById("master-filter");
+const masterOut = document.getElementById("master-out");
 
-let oscSine;
-let oscTri;
-let oscSquare;
-let oscSaw;
+const masterFilter = ctx.createBiquadFilter();
+const masterGainOut = ctx.createGain();
+masterFilter.type = "lowpass";
+masterFilter.frequency.value = 1000;
+masterFilter.connect(masterGainOut);
+masterGainOut.connect(ctx.destination);
 
-let bool = false;
+let oscSine, oscTri, oscSquare, oscSaw;
 
 class oscBuilder {
-	constructor(context, type, freq, choice, bool) {
-		this.context = context;
-		this.type = type;
-		this.freq = freq;
-		this.choice = choice;
+	constructor(audioContext, oscType, oscFreq, gainSlider) {
+		this.audioContext = audioContext;
+		this.oscType = oscType;
+		this.oscFreq = oscFreq;
+		this.gainSlider = gainSlider;
 	}
-
 	init() {
-		this.osc = this.context.createOscillator();
-		this.osc.type = this.type;
-		this.osc.frequency.value = this.freq;
-		this.amp = this.context.createGain();
+		this.osc = this.audioContext.createOscillator();
+		this.osc.type = this.oscType;
+		this.osc.frequency.value = this.oscFreq;
+		this.amp = this.audioContext.createGain();
 		this.osc.connect(this.amp);
 		this.amp.connect(masterFilter);
 	}
-
 	play() {
 		this.init();
 		this.slider();
-		this.osc.start(this.context.currentTime);
+		this.osc.start(this.audioContext.currentTime);
 	}
-
 	stop() {
-		this.osc.stop(this.context.currentTime)
+		this.osc.stop(this.audioContext.currentTime);
 	}
-
 	slider() {
 		setInterval(() => {
-			this.amp.gain.value = this.choice.value;
+			this.amp.gain.value = this.gainSlider.value;
 			// this.amp.gain.setValueAtTime(this.amp.gain.value, this.context.currentTime + 2);
 		}, 10)
 	}
 }
 
 function play() {
-		if(!bool) {
-			oscSine = new oscBuilder(ctx, 'sine', 261.63, sineRange);
+			oscSine = new oscBuilder(ctx, "sine", 261.63, sineRange);
 			oscSine.play();	
 
-			oscTri = new oscBuilder(ctx, 'triangle', 329.63,triRange);
+			oscTri = new oscBuilder(ctx, "triangle", 329.63,triRange);
 			oscTri.play();	
 
-			oscSquare = new oscBuilder(ctx, 'square', 392.00, squareRange);
+			oscSquare = new oscBuilder(ctx, "square", 392.00, squareRange);
 			oscSquare.play();
 	
-			oscSaw = new oscBuilder(ctx, 'sawtooth', 523.25,sawRange);
+			oscSaw = new oscBuilder(ctx, "sawtooth", 523.25,sawRange);
 			oscSaw.play();
-		}
  	}
-
 
 function stop() {
 	oscSine.stop();
@@ -78,15 +74,6 @@ function stop() {
 	oscSquare.stop();
 	oscSaw.stop();
 }
-
-const masterFilter = ctx.createBiquadFilter();
-const masterGainOut = ctx.createGain();
-masterFilter.type = "lowpass";
-masterFilter.frequency.value = 1000;
-masterFilter.connect(masterGainOut);
-
-
-masterGainOut.connect(ctx.destination);
 
 setInterval(() => {
 		masterFilter.frequency.value = masterFilt.value;
